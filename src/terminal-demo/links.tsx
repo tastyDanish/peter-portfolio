ba49cf1d60d18328763fc50d34b8ce337f549174
Peter Lansdaal <peter@gmail.com>
2025-06-11 17:06:38 -0700

    update: links to new loot box project

--- src/terminal-demo/links.tsx
+++ src/terminal-demo/links.tsx
@@ -17,23 +17,21 @@ const Links = () => {
 
       <br />
       <h2>Here are some lil projects I've built in my spare time</h2>
-      <h3>Actual Intelligence</h3>
+
+      <h3>Custom Lootbox</h3>
       <a
-        href="https://actual-intelligence.netlify.app/"
+        href="https://www.lootboxpicker.com/"
         target="_blank"
         rel="noopener noreferrer">
-        https://actual-intelligence.netlify.app/
+        https://www.lootboxpicker.com/
       </a>
       <p>
-        With the advent of LLM and AI Chatbots, I wanted to create a real,
-        authentic experience that mirrors the chat bot. This site lets you act
-        as both the User and the AI. As the user you get to make queries and
-        converse with the AI (actual Intelligence). Instead of using an LLM
-        chat, the user gets to role-play as the LLM!
-      </p>
-      <p>
-        This app utilizes supabase Realtime to broadcast chat messages to users
-        and implements a queue for assigning messages to AI users.
+        A playful decision-making tool built with React (Vite) and Framer
+        Motion. Users can add their own items to a virtual lootbox and "open" it
+        with a satisfying animation to randomly select one. The app uses local
+        storage to persist items between sessions, creating a lightweight but
+        memorable experience. Ideal for making everyday choices with a sense of
+        pride and accomplishment.
       </p>
 
       <h3>Fantasy Football Picker</h3>
